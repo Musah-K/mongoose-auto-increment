b45bb91ceb4e8169ee4185f3010750be5f65e289
Musah-K <elikarany@gmail.com>
2024-11-23 10:57:24 +0300

    first commit

---
+++ esm/index.js
@@ -0,0 +1,20 @@
+export default function autoIncrementField(schema, options) {
+    const fieldName = options && options.fieldName ? options.fieldName : 'index';
+  
+    const fieldConfig = {};
+    fieldConfig[fieldName] = { type: Number, unique: true, required: true };
+    schema.add(fieldConfig);
+  
+    schema.pre("validate", async function (next) {
+      if (this.isNew && !this[fieldName]) {
+        try {
+          const lastDoc = await this.constructor.findOne().sort({ [fieldName]: -1 }).exec();
+          this[fieldName] = lastDoc ? lastDoc[fieldName] + 1 : 1;
+        } catch (error) {
+          return next(error);
+        }
+      }
+      next();
+    });
+  }
+  
